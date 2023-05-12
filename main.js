09cd6fbb4ce8ef0aa8105edab2220c2a19f66495
Samuel2228 <sammycj22@gmail.com>
2023-05-12 15:15:46 +0100

    change

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let subject = document.getElementsByClassName('subject');
 let pTag = document.getElementById('paragraph');
 let checkBtn = document.getElementById('check');
+let body = document.getElementById('body');
 
 
 checkBtn.addEventListener('click', function() {
@@ -11,5 +12,23 @@ checkBtn.addEventListener('click', function() {
     let totalScore = Number(maths) + Number(eng) + Number(phy) + Number(chem);
     pTag.textContent = 'Your Jamb Score Is ' + totalScore;
 
+
+    if (totalScore >= 200) {
+        body.style.backgroundColor = 'green';
+        checkBtn.style.backgroundColor = 'green';
+        pTag.textContent += ' Boss!!!';
+    }
+
+    else if (totalScore >= 100 && totalScore < 200) {
+        body.style.backgroundColor = 'orange';
+        checkBtn.style.backgroundColor = 'orange';
+        pTag.textContent += ' You try';
+    }
+
+    else {
+        body.style.backgroundColor = 'red';
+        checkBtn.style.backgroundColor = 'red';
+        pTag.textContent += ' Just dey play';
+    }
 })
 
